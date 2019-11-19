2498154149b32ac21116a1dc16330951be8b7417
paullnh <plaird83@gmail.com>
2019-11-19 06:52:18 -0500

    Added action_template.json

--- assets/js/game/actions.js
+++ assets/js/game/actions.js
@@ -1,5 +1,7 @@
+const Template = require("./actions_template.json");
+
 class Ability {
-  constructor(name, cost, description, damage, block, ramp, duration, inititative) {
+  constructor({name, cost, description, damage, block, ramp, duration, inititative}) {
     this.name = name;
     this.cost = cost;
     this.description = description;
@@ -10,12 +12,11 @@ class Ability {
     this.inititative = inititative;
   }
 }
-
 module.exports = {
-  slash: new Ability("Slash", 2, "A slashing attack", 15, 0, 2, 1, 0),
-  block: new Ability("Block", 1, "Raise your shield to reduce damage", 0, 5, 1, 1, 0),
-  dodge: new Ability("Dodge", 2, "Move out of the way of the next attack", 0, 0, 0, -1, 0),
-  punch: new Ability("Punch", 1, "Punch opponent in the face", 5, 0, 1, 1, 0),
-  kick: new Ability("Kick", 1, "Front kick enemy in the chest", 10, 0, 1.5, 1, 0),
-  bodyslam: new Ability("Body Slam", 2, "Throw your opponent in the air then slamming them down onto the arena floor", 5, 0, 1, 2, 1)
+  slash: new Ability(Template.slash),
+  block: new Ability(Template.block),
+  dodge: new Ability(Template.dodge),
+  punch: new Ability(Template.punch),
+  kick: new Ability(Template.kick),
+  body_slam: new Ability(Template.body_slam)
 }
