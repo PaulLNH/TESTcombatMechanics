c14006ab653ce68d4623e4fc1cdbcde144f8f444
plaird730 <plaird730@gmail.com>
2019-11-20 17:31:33 -0500

    added strength talents to talents.js

--- assets/js/game/talents/talents.js
+++ assets/js/game/talents/talents.js
@@ -4,6 +4,8 @@ const talents = [
     title: "Slice",
     description:
       "Close range attack aimed at the opponent's forearms and hands, can be made with both the lead and back edges.",
+    applies: [],
+    gainAbility: ["slice"],
     maxPoints: 1,
     stats: [
       {
@@ -18,6 +20,8 @@ const talents = [
     dependsOn: [1],
     description:
       "Keep your weapons well honed increasing all weapon damage by {n}.",
+    applies: ["sharpening_stone"],
+    gainAbility: [],
     maxPoints: 1,
     stats: [
       {
@@ -32,6 +36,8 @@ const talents = [
     dependsOn: [1, 2],
     description:
       "Timed counter attack to deflect a thrust or parry a cut. The sword is hooked over the opponent's and forced downward and followed up with a thrust.",
+    applies: [],
+    gainAbility: ["setting_aside"],
     maxPoints: 1,
     stats: [
       {
@@ -44,7 +50,85 @@ const talents = [
     id: 4,
     title: "Serrated Blade",
     dependsOn: [1, 2, 3],
-    description: "Successful attacks with an edged weapon cause the opponent to bleed.",
+    description:
+      "Successful attacks with an edged weapon cause the opponent to bleed, this effect can stack up to 5 times.",
+    applies: ["serrated_blade"],
+    gainAbility: [],
+    maxPoints: 1,
+    stats: [
+      {
+        title: "Strength",
+        value: 1
+      }
+    ]
+  },
+  {
+    id: 5,
+    title: "Pommel",
+    dependsOn: [],
+    description: "Bash your opponent with the pommel of your sword.",
+    applies: [],
+    gainAbility: ["pommel"],
+    maxPoints: 1,
+    stats: [
+      {
+        title: "Strength",
+        value: 1
+      }
+    ]
+  },
+  {
+    id: 6,
+    title: "Parry",
+    dependsOn: [5],
+    description: "Parry your opponents attack.",
+    applies: ["parry"],
+    gainAbility: [],
+    maxPoints: 1,
+    stats: [
+      {
+        title: "Strength",
+        value: 1
+      }
+    ]
+  },
+  {
+    id: 7,
+    title: "Front Kick",
+    dependsOn: [5, 6],
+    description: "Front kick your opponent in the chest koncking them back.",
+    applies: [],
+    gainAbility: ["front_kick"],
+    maxPoints: 1,
+    stats: [
+      {
+        title: "Strength",
+        value: 1
+      }
+    ]
+  },
+  {
+    id: 8,
+    title: "Overpower",
+    dependsOn: [5, 6, 7],
+    description: "While locked in combat, overpower your opponent.",
+    applies: ["overpower"],
+    gainAbility: [],
+    maxPoints: 1,
+    stats: [
+      {
+        title: "Strength",
+        value: 1
+      }
+    ]
+  },
+  {
+    id: 9,
+    title: "Execute",
+    dependsOn: [1, 2, 3, 4, 5, 6, 7, 8],
+    description: "Deliver a fatal blow to your enemy.",
+    applies: [],
+    gainAbility: ["execute"],
     maxPoints: 1,
     stats: [
       {
